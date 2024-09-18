1be45d29c815ab9469d7c37a1604c4d4f89c669d
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-18 10:15:26 -0700

    import my nix lib

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "A Nix-flake-based Shell development environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nix-lib-monadam.url = "github:adam-neeley/nix-lib-monadam";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix-lib-monadam }:
     let
+      lib = nix-lib-monadam.lib;
       supportedSystems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
