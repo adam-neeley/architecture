cb949ee3a630b4fa120ef46d8a97366ef5a5da32
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-18 13:49:13 -0700

    update nix-lib-monadam

--- flake.nix
+++ flake.nix
@@ -7,20 +7,9 @@
   outputs = { self, nixpkgs, nix-lib-monadam }:
     let
       inherit (nix-lib-monadam) lib;
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          });
-
-      labs = lib.getDirs ./lab;
+      labs = lib.paths.getDirs ./lab;
     in {
-      packages = forEachSupportedSystem ({ pkgs }:
+      packages = lib.flakes.forAllSystems ({ pkgs }:
         pkgs.lib.attrsets.mergeAttrsList (map (lab: {
           "lab-${lab}" = pkgs.writeShellScriptBin "lab-${lab}" ''
             LABPATH=${./lab}/${lab}/
@@ -28,7 +17,7 @@
             ${pkgs.mars-mips}/bin/Mars p .
           '';
         }) labs));
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = lib.flakes.forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs;
             [
