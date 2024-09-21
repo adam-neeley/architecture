c6ec61457831b0b38fb74ebf4f47d653723cfbac
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-20 17:10:34 -0700

    format sqrt

--- extra/sqrt.asm
+++ extra/sqrt.asm
@@ -1,35 +1,35 @@
 # find sqrt(3) using Newton's method
 
         .data
-loop_msg:  .asciiz ":\t"
-break_msg: .asciiz "\n"
-x:         .float  2.0
-c1:        .float  0.5
-c2:        .float  1.5
-zero:      .float  0.0
+loop_msg:       .asciiz     ":\t"
+break_msg:      .asciiz     "\n"
+x:              .float      2.0
+c1:             .float      0.5
+c2:             .float      1.5
+zero:           .float      0.0
 
         .text
 main:
-    la         $s0, 20
-    lwc1       $f0, x
-    lwc1       $f2, c1
-    lwc1       $f4, c2
+    la          $s0,        20
+    lwc1        $f0,        x
+    lwc1        $f2,        c1
+    lwc1        $f4,        c2
 
 print:
-    li $v0, 1              # print iteration (int)
-    la $a0, ($s0)
+    li          $v0,        1            # print iteration (int)
+    la          $a0,        ($s0)
     syscall
 
-    li $v0, 4              # print string
-    la $a0, loop_msg
+    li          $v0,        4            # print string
+    la          $a0,        loop_msg
     syscall
 
-    li $v0, 2              # print float
-    mov.s $f12, $f0
+    li          $v0,        2            # print float
+    mov.s       $f12,       $f0
     syscall
 
-    li $v0, 4              # print string
-    la $a0, break_msg
+    li          $v0,        4            # print string
+    la          $a0,        break_msg
     syscall
 
 loop:
@@ -41,15 +41,15 @@ loop:
     #       = x - (0.5x - 1.5/x)
     #       = 0.5x + 1.5/x
 
-    mul.s $f8, $f0, $f2    # calc 0.5*x
-    div.s $f6, $f4, $f0    # calc 1.5/x
-    add.s $f0, $f8, $f6
+    mul.s       $f8,    $f0,    $f2    # calc 0.5*x
+    div.s       $f6,    $f4,    $f0    # calc 1.5/x
+    add.s       $f0,    $f8,    $f6
 
-    sub $s0, $s0, 1        # loop if iteration > 0
-    beq $s0, 0, exit
+    sub         $s0,    $s0,    1      # loop if iteration > 0
+    beq         $s0,    0,      exit
 
-    j print
+    j           print
 
 exit:
-    li $v0, 10
+    li          $v0,    10
     syscall
