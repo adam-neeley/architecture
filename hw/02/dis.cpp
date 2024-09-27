8cf7855e77f6005cd0d378082c75ec79661b0d7b
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-27 08:28:35 -0700

    hw 2

--- hw/02/dis.cpp
+++ hw/02/dis.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 static bool DEBUG = false;
 
-enum InstructionType { Register = 0, Immediate = 1, Jump = 2, Op = 3 };
+enum InstructionType { Register, Immediate, ImmediateUnsigned, Jump, Op };
 
 string to_string(InstructionType it) {
   switch (it) {
@@ -64,6 +64,7 @@ void dis(unsigned int instruction) {
   int funcNum = instruction & 0x3f;
   int pseudo_address = instruction & 0x1fffff;
   short immNum = (instruction) & 0xffff;
+  unsigned short immNumUnsigned = (instruction) & 0xffff;
   unsigned short immuNum = (instruction) & 0xffff;
 
   string op = op_table[opNum];
@@ -74,11 +75,15 @@ void dis(unsigned int instruction) {
   int other = otherNum;
   string func = func_table[funcNum];
   int imm = immNum;
+  int immUnsigned = immNumUnsigned;
 
   InstructionType it;
 
   if (op[opLastIndex] == 'i')
-    it = Immediate;
+    if (op[opLastIndex - 1] == 'u')
+      it = ImmediateUnsigned;
+    else
+      it = Immediate;
   else if (func == "syscall" || op == "jal")
     it = Op;
   else if (op[0] == 'j')
@@ -97,6 +102,7 @@ void dis(unsigned int instruction) {
     cout << "rs:     " << rs << endl;
     cout << "rt:     " << rt << endl;
     cout << "imm:    " << imm << endl;
+    cout << "immUn:  " << immUnsigned << endl;
     cout << "other:  " << other << endl;
     cout << "func:   " << func << " " << funcNum << endl;
     cout << "ps_add: " << pseudo_address << endl;
@@ -105,6 +111,9 @@ void dis(unsigned int instruction) {
   cout << "RESULT: \t";
   string spc = " ";
   switch (it) {
+  case ImmediateUnsigned:
+    cout << op << spc << rt << spc << rs << spc << immUnsigned << endl;
+    break;
   case Immediate:
     cout << op << spc << rt << spc << rs << spc << imm << endl;
     break;
@@ -130,20 +139,24 @@ void dis(unsigned int instruction) {
 }
 
 int main(int argc, char **argv) {
-  dis(0x2149ff9c);                         // addi $t1, $t2, -100
-  dis(0x014b4820);                         // add  $t1, $t2, $t3
-  dis(0x014b4822);                         // sub  $t1, $t2, $t3
+  dis(0x2149ff9c);                         // addi  $t1, $t2, -100
+  dis(0x014b4820);                         // add   $t1, $t2, $t3
+  dis(0x014b4821);                         // addu  $t1, $t2, $t3
+  dis(0x014b4822);                         // sub   $t1, $t2, $t3
+  dis(0x014b4823);                         // subu  $t1, $t2, $t3
   dis(0b00000000000000000000000000001100); // syscall
   dis(0b00000010001100010000100000001000); // jr
   dis(0b00110010001100010000000000000001); // andi 1
   dis(0b00101010011100110000000000000001); // slti
+  dis(0b00101110011100110000000000000001); // slti
   dis(0b00000010011101110001000000100100); // and
   dis(0b00000010011101110001000000100101); // or
   dis(0b00000010011101110001000000100110); // xor
   dis(0b00000010011101110001000000100111); // nor
   // + sltiu
   // + lui
-  // + addu
-  // + subu
   return 0;
 }
+
+// 1 3
+// a = 3
