b08b015dc8b1ab8f85da90e9d9edfba2e74dc6a2
monadam <aneeley@gmail.com>
2024-10-08 09:29:05 -0700

    dis

--- hw/02/dis.cpp
+++ hw/02/dis.cpp
@@ -11,26 +11,29 @@
 
 using namespace std;
 
-static bool DEBUG = true;
+static bool DEBUG = false;
+
 ostringstream debug_log;
 ostringstream results_log;
 
 enum instr_type { Register, ImmediateSigned, ImmediateUnsigned, Jump, OpUnary };
 
-static string instr_str[] = {"Register", "ImmediateSigned", "ImmediateUnsigned",
+static const string instr_str[] = {"Register", "ImmediateSigned", "ImmediateUnsigned",
                              "Jump", "OpUnary"};
 
-static string op_table[] = {"",     "",     "j",    "jal",   "beq",  "bne",
+static const string op_table[] = {"",     "",     "j",    "jal",   "beq",  "bne",
                             "blez", "bgtz", "addi", "addiu", "slti", "sltiu",
                             "andi", "ori",  "xori", "lui"};
-static string func_table[] = {
+
+static const string func_table[] = {
     "sll",  "",      "srl",  "sra",  "sllv",    "",      "srlv", "srav",
     "jr",   "jalr",  "movz", "movn", "syscall", "break", "",     "sync",
     "mfhi", "mfhi",  "mflo", "mflo", "",        "",      "",     "",
     "mult", "multu", "div",  "divu", "",        "",      "",     "",
     "add",  "addu",  "sub",  "subu", "and",     "or",    "xor",  "nor",
     "",     "",      "slt",  "sltu", "",        ""};
-static string reg_table[] = {"zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
+
+static const string reg_table[] = {"zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
                              "t0",   "t1", "t2", "t3", "t4", "t5", "t6", "t7",
                              "s0",   "s1", "s2", "s3", "s4", "s5", "s6", "s7",
                              "t8",   "t9", "k0", "k1", "gp", "sp", "fp", "ra"};
@@ -42,6 +45,7 @@ string str_instr(vector<string> strs) {
     res << str << setw(5) << left;
   return res.str();
 }
+
 void dis(unsigned int instruction) {
   // R: [  op   | rs  | rt  | rd  |other| func ]
   // I: [  op   | rs  | rt  |       imm        ]
